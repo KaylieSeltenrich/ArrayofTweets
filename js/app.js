8bb90ebb8de972d2687dec2ce7af80e28b13d6e0
orcinusorca1995 <kaylie.seltenrich@gmail.com>
2020-08-20 20:16:24 -0600

    Array of Tweets W9C Assignment

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var tweets = ["Hello","Hi","What's up","Cool","Same here","Lol","Neat","Nice","Howdy","Yay"];
+
+for (var counter = 0; counter < tweets.length; counter++)
+{ console.log(tweets[counter]);
+}
